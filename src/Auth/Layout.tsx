8ebd9c7a7d9bf5b07819c8bb23a44f6c83c7343d
Sharkolly <sharkollymofeoluwa@gmail.com>
2025-02-18 20:12:43 +0100

    new update on tailwind

--- src/Auth/Layout.tsx
+++ src/Auth/Layout.tsx
@@ -7,7 +7,7 @@ type ChildrenProp =  {
 const Layout = ({children}: ChildrenProp) => {
   return (
     <div className='bg-linear-gradient text-black flex justify-center items-center h-[100vh]'>
-        <div className='shadow-2xl bg-white shadow-blue-300/100 border-2 w-[27%] mx-auto rounded-xl pt-7 pb-12 max-xl:w-[50%] max-lg:w-[70%] max-md:w-[96%] max-md:py-8'>
+        <div className='shadow-2xl bg-white shadow-blue-300/100 border-2 w-[30%] mx-auto rounded-xl pt-7 pb-12 max-xl:w-[50%] max-lg:w-[70%] max-md:w-[96%] max-md:py-8'>
           {children}
         </div>
     </div>
