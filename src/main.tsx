0ddeddd85dccffbbe3aa9d221680885a9ba172fd
Sharkolly <sharkollymofeoluwa@gmail.com>
2025-02-18 19:43:39 +0100

    new update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Context from './Store/context.tsx'
+import Context from './Store/Context.tsx'
 // import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
