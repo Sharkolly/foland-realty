dc93d630b0aaaa3c9282430e8bd04d02f01f2d6d
Sharkolly <sharkollymofeoluwa@gmail.com>
2025-02-19 20:50:12 +0100

    update on the nav

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,14 @@
+import { NavLink } from "react-router-dom"
+
 const Home = () => {
   return (
     <div>
       Hello WOrld
+
+      <NavLink to='/login'>Login</NavLink>
+      <NavLink to='/signup'>Sign up</NavLink>
+      <NavLink to='/about'>About</NavLink>
+      <NavLink to='/admin'>Admin</NavLink>
     </div>
   )
 }
