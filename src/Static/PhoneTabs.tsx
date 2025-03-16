3631f72da11abb200c4a70ce97dfff51d1ea9c13
Sharkolly <sharkollymofeoluwa@gmail.com>
2025-03-16 12:22:23 +0100

    new commit

--- src/Static/PhoneTabs.tsx
+++ src/Static/PhoneTabs.tsx
@@ -9,7 +9,7 @@ const PhoneTabs = () => {
   return (
     <div>
       <div className="fixed bottom-0 left-0 right-0 bg-slate-300 py-4.5">
-        <div className=" flex justify-center items-center gap-11 max-[400px]:gap-9 max-[360px]:gap-5  w-[91%] mx-auto">
+        <div className=" flex justify-center items-center max-[580px]:gap-14 max-[400px]:gap-11 max-[400px]:gap-9 max-[360px]:gap-5  w-[91%] mx-auto">
           <NavLink
             className={({ isActive }) =>
               isActive
@@ -21,7 +21,7 @@ const PhoneTabs = () => {
             <div>
               <FaHome />
             </div>
-            <p className="text-[.9em]">Home</p>
+            <p className="">Home</p>
           </NavLink>
           <NavLink
             className={({ isActive }) =>
@@ -34,7 +34,7 @@ const PhoneTabs = () => {
             <div>
               <PiHouseLineFill />
             </div>
-            <p className="text-[.9em]">Properties</p>
+            <p className="">Properties</p>
           </NavLink>
           <NavLink
             className={({ isActive }) =>
